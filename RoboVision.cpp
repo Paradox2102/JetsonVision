f5bc70b8a9dc557cd678f9f7d47da8eed34ec49f
Rohit Vighne <rohit.vighne@gmail.com>
2016-12-28 18:52:36 -0800

    Correct data for target center lines sent to robot

--- RoboVision.cpp
+++ RoboVision.cpp
@@ -17,6 +17,9 @@ class SAMVisionServer final : public ImageServer
 	using Contour = std::vector<cv::Point>;
 	using ContourArray = std::vector<Contour>;
 
+	static constexpr int frameWidth = 640;
+	static constexpr int frameHeight = 480;
+
 	static constexpr bool rotateHSV = false; // true = better for red
 
 	static constexpr double polyEpsilon = 10.0;
@@ -28,12 +31,45 @@ class SAMVisionServer final : public ImageServer
 
 	const std::string saveFilename;
 
+	// Default values
 	ImageHeader header
 	{
-		0,
-		0, 255,
-		0, 255,
-		0, 255,
+		0,	// uint32_t imageSize;
+		0,	// int16_t MinH;
+		255,	// int16_t MaxH;
+		0,	// int16_t MinS;
+		255,	// int16_t MaxS;
+		0,	// int16_t MinV;
+		255,	// int16_t MaxV;
+		0,	// int16_t cameraBrightness;
+		0,	// int16_t cameraSaturation;
+		0,	// int16_t cameraISO;
+		frameHeight / 2,	// int16_t targetPosition;
+		0,	// int16_t pixelX;
+		0,	// int16_t pixelY;
+		0,	// int16_t pixelRed;
+		0,	// int16_t pixelBlue;
+		0,	// int16_t pixelGreen;
+		0,	// int16_t pixelHue;
+		0,	// int16_t pixelSat;
+		0,	// int16_t pixelVal;
+		0,	// int16_t targetX;
+		0,	// int16_t targetY;
+		0,	// int16_t targetWidth;
+		0,	// int16_t targetHeight;
+		0,	// float camRedBalance;
+		0,	// float camBlueBalance;
+		0,	// int16_t camAutoColorBalance;
+		0,	// int16_t camExposureComp;
+		0,	// int16_t camContrast;
+		0,	// int16_t minArea;
+		0,	// int16_t camShutterSpeed;
+		0,	// float fps;
+		0,	// int16_t showFiltered;
+		0,	// int16_t zoom;
+		0,	// int16_t camAutoExposure;
+		0,	// int16_t blobPosition;
+		frameWidth / 2,	// int16_t centerLine;
 	};
 
 	cv::Scalar thresholdMin, thresholdMax;
@@ -102,18 +138,25 @@ public:
 
 			if (quad.size() == 4)
 			{
-				auto distLine = std::max(quad[0].y, quad[3].y);
-				auto midLine = (quad[0].x + quad[3].x) / 2;
+				int distLine = std::max(quad[0].y, quad[3].y);
+				int midLine = (quad[0].x + quad[3].x) / 2;
 
 				cv::line(frame, { 0, distLine }, { frame.cols, distLine }, contourColor);
 				cv::line(frame, { midLine, 0 }, { midLine, frame.rows }, contourColor);
 
 				// cv::drawContours(frame, contours, -1, { 0, 255, 0 });
 
-				auto rect = cv::boundingRect(*pWinningContour);
+				cv::Rect rect = cv::boundingRect(*pWinningContour);
 				cv::rectangle(frame, rect.tl(), rect.br(), contourColor, contourThickness);
 
-				dataServer.bufferData('R', { rect.x, rect.y, rect.width, rect.height, quad[0].y, quad[3].y, distLine, ++nFrames, midLine });
+				dataServer.bufferData('R', {
+					rect.x, rect.y,
+					rect.width, rect.height,
+					quad[0].y, quad[3].y,
+					header.targetPosition,
+					++nFrames,
+					header.centerLine
+				});
 				dataServer.run(17);
 			}
 		}
